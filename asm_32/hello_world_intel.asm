9efd1bbd2b00a69f82484a4d1a6f7fbce3aafbcb
Daniel Romero <infoslack@gmail.com>
2014-01-19 19:54:57 -0300

    add doc for hello_world_intel.asm

--- asm_32/hello_world_intel.asm
+++ asm_32/hello_world_intel.asm
@@ -8,19 +8,19 @@ _start:
 
   starter:
 
-  xor eax, eax
-  xor ebx, ebx
-  xor edx, edx
-  xor ecx, ecx
-  mov al, 4
-  mov bl, 1
-  pop ecx
-  mov dl, 11
-  int 0x80
-  xor eax, eax
-  mov al, 1
-  xor ebx, ebx
-  int 0x80
+  xor eax, eax    ; null eax
+  xor ebx, ebx    ; null ebx
+  xor edx, edx    ; null edx
+  xor ecx, ecx    ; null ecx
+  mov al, 4       ; execute syscall (write)
+  mov bl, 1       ; activate stdout
+  pop ecx         ; copies the address of the string and adds the stack
+  mov dl, 11      ; lenght of the string (Hello World)
+  int 0x80        ; call kernel exit
+  xor eax, eax    ; null eax
+  mov al, 1       ; exit(0)
+  xor ebx, ebx    ; null ebx
+  int 0x80        ; call kernel exit
   ender:
-  call starter
+  call starter    ; put the address of the string on the stack
   db 'Hello World'
